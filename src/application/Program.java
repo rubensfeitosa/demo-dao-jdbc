1e96d3246715d67cbc1fbe1d771a166848a70e83
rubensfeitosa <rubens.feitosa3@gmail.com>
2025-02-05 11:56:23 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,46 +6,13 @@ import java.sql.Statement;
 
 import db.DB;
 import db.DbException;
+import model.entities.Department;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Connection conn = null;
-		Statement st = null;
-		try {
-			conn = DB.getConnection();
-	
-			conn.setAutoCommit(false);
-
-			st = conn.createStatement();
-			
-			int rows1 = st.executeUpdate("UPDATE seller SET BaseSalary = 1000 WHERE DepartmentId = 1");
-
-			//int x = 1;
-			//if (x < 2) {
-			//	throw new SQLException("Fake error");
-			//}
-			
-			int rows2 = st.executeUpdate("UPDATE seller SET BaseSalary = 1500 WHERE DepartmentId = 2");
-			
-			conn.commit();
-			
-			System.out.println("rows1 = " + rows1);
-			System.out.println("rows2 = " + rows2);
-		}
-		catch (SQLException e) {
-			try {
-				conn.rollback();
-				throw new DbException("Transaction rolled back! Caused by: " + e.getMessage());
-			} 
-			catch (SQLException e1) {
-				throw new DbException("Error trying to rollback! Caused by: " + e1.getMessage());
-			}
-		} 
-		finally {
-			DB.closeStatement(st);
-			DB.closeConnection();
-		}
+		
+		Department obk = new Department(1, "livros");
+		System.out.println(obk);
 	}
 }
