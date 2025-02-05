28f9dff02f89ef31ca903c8868dc447460337375
rubensfeitosa <rubens.feitosa3@gmail.com>
2025-02-05 20:38:26 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,6 +7,7 @@ import java.util.Date;
 
 import db.DB;
 import db.DbException;
+import model.dao.DaoFactory;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -15,6 +16,8 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Department obk = new Department(1, "livros");
+		
+		SelllerDao sellerDao = DaoFactory.createSellerDao();
 		
 		Seller seller = new Seller(32, "Rubens", "teste@teste.com", new Date(), 3000.0, obk);
 		System.out.println(seller);
