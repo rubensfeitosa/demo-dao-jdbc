13814c38ca6a595207919a84e6023a42056fc660
rubensfeitosa <rubens.feitosa3@gmail.com>
2025-02-05 20:18:55 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,16 +3,20 @@ package application;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 import db.DB;
 import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obk = new Department(1, "livros");
-		System.out.println(obk);
+		
+		Seller seller = new Seller(32, "Rubens", "teste@teste.com", new Date(), 3000.0, obk);
+		System.out.println(seller);
 	}
 }
