f6906f9f7354d3df39aeae88c91b673254d37fa2
rubensfeitosa <rubens.feitosa3@gmail.com>
2025-02-06 20:55:14 -0300

    Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,17 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 4: seller insert =====");
+		Seller newSeller = new Seller(null, "Rubens", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Rogerio");
+		sellerDao.update(seller);
+		System.out.println("Update conccluido");
+		
 	}
 }
